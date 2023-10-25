df3dfad26bfa5a1c35af3b38ebfcd23051bdac1b
GrowlyX <growlyxdev@gmail.com>
2023-10-25 17:00:07 -0400

    fix: fix config variables not working properly

--- TeamCode/src/main/java/org/riverdell/robotics/xdk/opmodes/pipeline/AutoPipelineUtilities.kt
+++ TeamCode/src/main/java/org/riverdell/robotics/xdk/opmodes/pipeline/AutoPipelineUtilities.kt
@@ -5,12 +5,7 @@ import com.acmerobotics.dashboard.config.Config
 @Config
 object AutoPipelineUtilities
 {
-    @JvmStatic
-    var UNITS_PER_INCH_STRAFE = 153.5
-
-    @JvmStatic
-    var UNITS_PER_INCH_FORWARD = 89.127
-
-    @JvmStatic
-    var UNITS_PER_DEGREE_TURN = 21.111
+    @JvmField var UNITS_PER_INCH_STRAFE = 153.5
+    @JvmField var UNITS_PER_INCH_FORWARD = 89.127
+    @JvmField var UNITS_PER_DEGREE_TURN = 21.111
 }
