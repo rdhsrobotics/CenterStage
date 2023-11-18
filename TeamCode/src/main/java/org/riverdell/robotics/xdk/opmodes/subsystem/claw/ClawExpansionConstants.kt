2dc2dc07ce66263620469a894ee411fc92d09660
Subham <growlyxdev@gmail.com>
2023-11-18 12:37:03 -0500

    feat: expansion updates

--- TeamCode/src/main/java/org/riverdell/robotics/xdk/opmodes/subsystem/claw/ClawExpansionConstants.kt
+++ TeamCode/src/main/java/org/riverdell/robotics/xdk/opmodes/subsystem/claw/ClawExpansionConstants.kt
@@ -9,11 +9,11 @@ object ClawExpansionConstants
     @JvmField var INTERMEDIATE_EXTENDER_POSITION = 0.13
     @JvmField var MAX_EXTENDER_POSITION = 0.28
 
-    @JvmField var CLOSED_LEFT_CLAW = 0.26
-    @JvmField var OPEN_LEFT_CLAW = 0.45
+    @JvmField var CLOSED_LEFT_CLAW = 0.5
+    @JvmField var OPEN_LEFT_CLAW = 0.7
 
-    @JvmField var CLOSED_RIGHT_CLAW = 0.8
-    @JvmField var OPEN_RIGHT_CLAW = 0.6
+    @JvmField var CLOSED_RIGHT_CLAW = 1.0
+    @JvmField var OPEN_RIGHT_CLAW = 0.8
 
     @JvmField var DEFAULT_PLANE_POSITION = 0.6
     @JvmField var MAX_PLANE_POSITION = 1.0
