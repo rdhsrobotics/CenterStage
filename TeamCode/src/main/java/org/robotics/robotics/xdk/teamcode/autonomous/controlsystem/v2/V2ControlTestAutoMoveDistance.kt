7631404a082df8352a346d7fc9e2ca3d8aaf4d98
GrowlyX <growlyxdev@gmail.com>
2024-01-07 19:02:29 -0500

    chore: remove imports

--- TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/controlsystem/v2/V2ControlTestAutoMoveDistance.kt
+++ TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/controlsystem/v2/V2ControlTestAutoMoveDistance.kt
@@ -1,12 +1,8 @@
 package org.robotics.robotics.xdk.teamcode.autonomous.controlsystem.v2
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import io.liftgate.robotics.mono.Mono
 import io.liftgate.robotics.mono.pipeline.single
 import org.robotics.robotics.xdk.teamcode.autonomous.AbstractAutoPipeline
-import org.robotics.robotics.xdk.teamcode.autonomous.contexts.ClawSubsystemContext
-import org.robotics.robotics.xdk.teamcode.autonomous.detection.TapeSide
-import org.robotics.robotics.xdk.teamcode.autonomous.detection.TeamColor
 import org.robotics.robotics.xdk.teamcode.autonomous.profiles.AutonomousProfile
 
 @Autonomous(name = "Test | Move Distance", group = "Test")
