886a93ed26a9d94d3ae023bb0219b1879ea72743
GrowlyX <growlyxdev@gmail.com>
2024-01-23 18:44:49 -0500

    chore: rename autons to close/far

--- TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/AutoPipelineImpls.kt
+++ TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/AutoPipelineImpls.kt
@@ -6,56 +6,56 @@ import org.robotics.robotics.xdk.teamcode.Global
 import org.robotics.robotics.xdk.teamcode.autonomous.profiles.AutonomousProfile
 
 @Autonomous(
-    name = "Red | Player 2 | 2+0F",
+    name = "Red | Far | 2+0F",
     group = "RedFast",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
 class AutoPipelineRedPlayer2 : AbstractAutoPipeline(AutonomousProfile.RedPlayer2TwoPlusZero)
 
 @Autonomous(
-    name = "Red | Player 2 | 2+0S",
+    name = "Red | Far | 2+0S",
     group = "RedSlow",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
 class AutoPipelineRedPlayer2SlowMode : AbstractAutoPipeline(
     AutonomousProfile.RedPlayer2TwoPlusZero,
     blockExecutionGroup = { k, v ->
-        waitMillis(5000L)
+        waitMillis(3000L)
         AutonomousProfile.RedPlayer2TwoPlusZero
             .buildExecutionGroup()(k, v)
     }
 )
 
 @Autonomous(
-    name = "Red | Player 1 | 2+0F",
+    name = "Red | Close | 2+0F",
     group = "RedFast",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
 class AutoPipelineRedPlayer1 : AbstractAutoPipeline(AutonomousProfile.RedPlayer1TwoPlusZero)
 
 @Autonomous(
-    name = "Blue | Player 2 | 2+0F",
+    name = "Blue | Far | 2+0F",
     group = "BlueFast",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
 class AutoPipelineBluePlayer2 : AbstractAutoPipeline(AutonomousProfile.BluePlayer2TwoPlusZero)
 
 @Autonomous(
-    name = "Blue | Player 2 | 2+0S",
+    name = "Blue | Far | 2+0S",
     group = "BlueSlow",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
 class AutoPipelineBluePlayer2SlowMode : AbstractAutoPipeline(
     AutonomousProfile.BluePlayer2TwoPlusZero,
     blockExecutionGroup = { k, v ->
-        waitMillis(5000L)
+        waitMillis(3000L)
         AutonomousProfile.BluePlayer2TwoPlusZero
             .buildExecutionGroup()(k, v)
     }
 )
 
 @Autonomous(
-    name = "Blue | Player 1 | 2+0F",
+    name = "Blue | Close | 2+0F",
     group = "BlueFast",
     preselectTeleOp = Global.RobotCentricTeleOpName
 )
