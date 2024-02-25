e5578e0315e0f7b80e684c649c0ac3a2acbca217
Subham <growlyxdev@gmail.com>
2024-02-24 20:11:56 -0500

    chore: revert to prev scaling cuz i dn't wanna mess wit it LAWL

--- TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/AbstractTeleOp.kt
+++ TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/AbstractTeleOp.kt
@@ -73,11 +73,11 @@ abstract class AbstractTeleOp : LinearOpMode(), System
         while (opModeIsActive())
         {
             /*
-            val multiplier = 0.5 + gamepad1.right_trigger * 0.5
-             */
             val multiplier = MathUtils.INSTANCE.joystickScalar(
                 gamepad1.right_trigger.toDouble(), 0.01
             )
+             */
+            val multiplier = 0.5 + gamepad1.right_trigger * 0.5
 
             driveRobot(drivebase, driverOp, multiplier)
 
