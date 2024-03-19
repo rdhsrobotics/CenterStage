a3cf6860e771557ee24bba3a65cfafd9e581adcb
Subham <growlyxdev@gmail.com>
2024-03-19 19:18:34 -0400

    fix: add geo utils

---
+++ TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/geometry/HermitePose.java
@@ -0,0 +1,35 @@
+package org.robotics.robotics.xdk.teamcode.autonomous.geometry;
+
+public class HermitePose {
+    public double x, y;
+    public Vector2D tangent;
+
+    public HermitePose(double x, double y, Vector2D tangent) {
+        this.x = x;
+        this.y = y;
+        this.tangent = tangent;
+    }
+
+    public HermitePose(double x, double y, double heading) {
+        this.x = x;
+        this.y = y;
+        this.tangent = new Vector2D(Math.cos(heading), Math.sin(heading));
+    }
+
+    public Point add(HermitePose other) {
+        return new Point(x + other.x, y + other.y);
+    }
+
+    public Point subt(HermitePose other) {
+        return new Point(x - other.x, y - other.y);
+    }
+
+    public Pose pose() {
+        return new Pose(x, y, Math.atan2(y, x));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.2f, %.2f, %s", x, y, tangent);
+    }
+}
---
+++ TeamCode/src/main/java/org/robotics/robotics/xdk/teamcode/autonomous/geometry/Spline.java
@@ -0,0 +1,37 @@
+package org.robotics.robotics.xdk.teamcode.autonomous.geometry;
+
+public class Spline {
+    private final Polynomial x;
+    private final Polynomial y;
+
+    public Spline(Polynomial x, Polynomial y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Pose calculate(double t, int n) {
+        return new Pose(x.calculate(t, n), y.calculate(t, n), 0.0);
+    }
+
+    public double curvature(double t) {
+        double x_prime = x.calculate(t, 1);
+        double y_prime = y.calculate(t, 1);
+
+        double kNum = Math.abs((y.calculate(t, 2) * x_prime) - y_prime * x.calculate(t, 2));
+        double kDom = Math.pow((x_prime * x_prime) + (y_prime * y_prime), 1.5);
+
+        return kNum / kDom;
+    }
+
+    public double getHeading(double t) {
+        return Math.atan2(y.calculate(t, 1), x.calculate(t, 1));
+    }
+
+    public Polynomial getX() {
+        return x;
+    }
+
+    public Polynomial getY() {
+        return y;
+    }
+}
